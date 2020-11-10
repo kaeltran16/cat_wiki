24d9595ea6f2d7f02a4e8ade6f1a7777a9d01b3a
kael <piskael03@gmail.com>
2020-11-10 18:35:29 +0700

    add .idea to gitignore

---
+++ packages/web/build/precache-manifest.f01a27492760179615051fa0b8a30bd3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c0515c061f70383f22d43aecb4e5a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f846a0a16fd167395e4",
+    "url": "/static/css/2.576ce916.chunk.css"
+  },
+  {
+    "revision": "9f846a0a16fd167395e4",
+    "url": "/static/js/2.7ad01598.chunk.js"
+  },
+  {
+    "revision": "a3ca4fad6c20f8df27c31f653f9e89e9",
+    "url": "/static/js/2.7ad01598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebc7494b1550eac2df4",
+    "url": "/static/js/main.932fc9ce.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "3a811f3349d8087677efa6a8800d3e28",
+    "url": "/static/media/HeroImagemd.3a811f33.png"
+  },
+  {
+    "revision": "dd9bd09d656b87ad0b432ac5d3f06a00",
+    "url": "/static/media/image 1.dd9bd09d.png"
+  },
+  {
+    "revision": "f948b5937b8d27c234881c7d62312e17",
+    "url": "/static/media/image 2.f948b593.png"
+  },
+  {
+    "revision": "1a3dc5d5a0ac9e36b4fdb081a0d270cf",
+    "url": "/static/media/image 3.1a3dc5d5.png"
+  }
+]);
